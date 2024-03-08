32daff33697d7428325634acc94db0438599ffb5
warsiKamran <kamranwarsi.kw@gmail.com>
2024-03-08 20:12:03 +0530

    added aggregation pipelines

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -324,3 +324,65 @@ export const updateCoverImage = asyncHandler(async(req, res) => {
     );
 });
 
+
+export const getUserChannelProfile = asyncHandler(async(req, res) => {
+
+    const {username} = req.params;
+
+    if(!username?.trim()){
+        throw new apiError(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",    // Subscription will change to subscriptions
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup :{
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullname: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                email: 1,
+                avatar: 1,
+                coverImage: 1
+            }
+        }
+    ]);
+});
